698d24689968a00bb5d423783fd50d7e50e2b741
sklbz <sokol.bozanic@tumo.world>
2026-04-06 22:20:42 +0200

    there are some problems, working on it

--- SardLemma/Basic.lean
+++ SardLemma/Basic.lean
@@ -1,1 +1,22 @@
-def hello := "world"
+import Mathlib.Analysis.Calculus.ContDiff.Basic
+import Mathlib.Analysis.Calculus.Deriv.Basic
+import Mathlib.Data.Real.Basic
+import Mathlib.Order.Interval.Set.Defs
+import Mathlib.Analysis.Series.Basic
+open BigOperators
+
+def majore (ε : ℝ) (u : ℕ → ℝ) : Prop :=
+  Summable u ∧ ∑' n, u n ≤ ε
+
+-- Définition d'un ensemble de mesure nulle
+def is_negligeable (A : Set ℝ) : Prop :=
+  ∀ ε >0 ,
+  (∃ (a: ℕ → ℝ), ∃ (b: ℕ → ℝ), (∀ n, (a n)≤ (b n)) ∧ 
+  (A ⊆ ⋃ n, Set.Icc (a n) (b n)) ∧
+  (majore ε (a - b)))
+
+
+theorem sard_lemma (f : ℝ → ℝ) (hf : ContDiff ℝ 1 f) : 
+  is_negligeable (f '' {x | deriv f x = 0}) := 
+by 
+  sorry
