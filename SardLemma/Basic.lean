83d9b8f9363a9b6da19b4b03adaf7c48cd7099d1
sklbz <sokol.bozanic@tumo.world>
2026-04-07 22:07:14 +0200

    progress made

--- SardLemma/Basic.lean
+++ SardLemma/Basic.lean
@@ -2,15 +2,12 @@ import Mathlib.Analysis.Calculus.ContDiff.Basic
 import Mathlib.Analysis.Calculus.ContDiff.Deriv
 import Mathlib.Analysis.Calculus.Deriv.Basic
 import Mathlib.Topology.UniformSpace.HeineCantor
+import Mathlib.Topology.MetricSpace.Basic
 import Mathlib.Data.Real.Basic
 import Mathlib.Order.Interval.Set.Defs
 import Mathlib.Data.Finset.Defs
 
 open BigOperators
-
-example (I : Set ℝ) (f : ℝ → ℝ) (f_cont : ContinuousOn f I) (hI : IsCompact I) : 
-    UniformContinuousOn f I := by
-  exact IsCompact.uniformContinuousOn_of_continuous hI f_cont
 
 -- Définition d'un ensemble de mesure nulle
 def is_negligeable (A : Set ℝ) : Prop :=
@@ -19,10 +16,11 @@ def is_negligeable (A : Set ℝ) : Prop :=
   (A ⊆ ⋃ n, Set.Icc (a n) (b n)) ∧
   (∀ n : ℕ, (∑ k ∈ Finset.range (n+1), (b k - a k)) ≤ ε)
 
-lemma sard_lemma_compact (a b : ℝ) (f : ℝ → ℝ) (hf : ContDiff ℝ 1 f) : 
+lemma sard_lemma_compact (a b : ℝ) (f : ℝ → ℝ) (hμ: b-a > 0) (hf : ContDiff ℝ 1 f) : 
   is_negligeable (f '' {x ∈ Set.Icc a b | deriv f x = 0}) := 
 by
   let I : Set ℝ := Set.Icc a b
+  let μ := b-a
   let f' : ℝ → ℝ := deriv f
 
   have hf'_cont : Continuous f' := ContDiff.continuous_deriv_one hf
@@ -32,10 +30,23 @@ by
 
   have hf'_uniform : UniformContinuousOn f' I := 
     IsCompact.uniformContinuousOn_of_continuous hI_compact hf'_cont_on_I
-  -- hε : ε > 0
+
+  rw [Metric.uniformContinuousOn_iff] at hf'_uniform
+
   intro ε hε
 
-  obtain ⟨δ, δ_pos, hδ⟩ := hf'_uniform ε hε
+  let ε' := ε / μ
+
+  let hε' := div_pos hε hμ
+
+  obtain ⟨δ, δ_pos, hδ⟩ := hf'_uniform ε' hε'
+
+  let k := ⌈μ / δ⌉
+  have hk : (k: ℝ) > 0 := Int.cast_pos.2 (Int.ceil_pos.2 (div_pos hμ δ_pos))
+
+  let δ' := μ / k
+  have δ'_pos := div_pos δ_pos hk
+
   sorry
 
 theorem sard_lemma (f : ℝ → ℝ) (hf : ContDiff ℝ 1 f) : 
