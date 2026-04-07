c532b4ca69dc9fe470c3a6d6d0120c7fa50d1895
sklbz <sokol.bozanic@tumo.world>
2026-04-07 21:38:22 +0200

    advancing on the compact case

--- SardLemma/Basic.lean
+++ SardLemma/Basic.lean
@@ -1,4 +1,5 @@
 import Mathlib.Analysis.Calculus.ContDiff.Basic
+import Mathlib.Analysis.Calculus.ContDiff.Deriv
 import Mathlib.Analysis.Calculus.Deriv.Basic
 import Mathlib.Topology.UniformSpace.HeineCantor
 import Mathlib.Data.Real.Basic
@@ -6,6 +7,10 @@ import Mathlib.Order.Interval.Set.Defs
 import Mathlib.Data.Finset.Defs
 
 open BigOperators
+
+example (I : Set ℝ) (f : ℝ → ℝ) (f_cont : ContinuousOn f I) (hI : IsCompact I) : 
+    UniformContinuousOn f I := by
+  exact IsCompact.uniformContinuousOn_of_continuous hI f_cont
 
 -- Définition d'un ensemble de mesure nulle
 def is_negligeable (A : Set ℝ) : Prop :=
@@ -19,8 +24,18 @@ lemma sard_lemma_compact (a b : ℝ) (f : ℝ → ℝ) (hf : ContDiff ℝ 1 f) :
 by
   let I : Set ℝ := Set.Icc a b
   let f' : ℝ → ℝ := deriv f
+
+  have hf'_cont : Continuous f' := ContDiff.continuous_deriv_one hf
+  have hf'_cont_on_I : ContinuousOn f' I := hf'_cont.continuousOn
+
+  have hI_compact : IsCompact I := isCompact_Icc
+
+  have hf'_uniform : UniformContinuousOn f' I := 
+    IsCompact.uniformContinuousOn_of_continuous hI_compact hf'_cont_on_I
   -- hε : ε > 0
   intro ε hε
+
+  obtain ⟨δ, δ_pos, hδ⟩ := hf'_uniform ε hε
   sorry
 
 theorem sard_lemma (f : ℝ → ℝ) (hf : ContDiff ℝ 1 f) : 
