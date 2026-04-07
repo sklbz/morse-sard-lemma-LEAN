b52d19abe905e2058d3d9627e2b5a6eefe44944e
sklbz <sokol.bozanic@tumo.world>
2026-04-07 20:52:39 +0200

    starting the proof for the restriction to a compact domain

--- SardLemma/Basic.lean
+++ SardLemma/Basic.lean
@@ -1,20 +1,27 @@
 import Mathlib.Analysis.Calculus.ContDiff.Basic
 import Mathlib.Analysis.Calculus.Deriv.Basic
+import Mathlib.Topology.UniformSpace.HeineCantor
 import Mathlib.Data.Real.Basic
 import Mathlib.Order.Interval.Set.Defs
-import Mathlib.Analysis.Series.Basic
-open BigOperators
+import Mathlib.Data.Finset.Defs
 
-def majore (ε : ℝ) (u : ℕ → ℝ) : Prop :=
-  Summable u ∧ ∑' n, u n ≤ ε
+open BigOperators
 
 -- Définition d'un ensemble de mesure nulle
 def is_negligeable (A : Set ℝ) : Prop :=
-  ∀ ε >0 ,
-  (∃ (a: ℕ → ℝ), ∃ (b: ℕ → ℝ), (∀ n, (a n)≤ (b n)) ∧ 
+  ∀ ε > 0,
+  ∃ (a b: ℕ → ℝ), (∀ n, a n ≤ b n) ∧ 
   (A ⊆ ⋃ n, Set.Icc (a n) (b n)) ∧
-  (majore ε (a - b)))
+  (∀ n : ℕ, (∑ k ∈ Finset.range (n+1), (b k - a k)) ≤ ε)
 
+lemma sard_lemma_compact (a b : ℝ) (f : ℝ → ℝ) (hf : ContDiff ℝ 1 f) : 
+  is_negligeable (f '' {x ∈ Set.Icc a b | deriv f x = 0}) := 
+by
+  let I : Set ℝ := Set.Icc a b
+  let f' : ℝ → ℝ := deriv f
+  -- hε : ε > 0
+  intro ε hε
+  sorry
 
 theorem sard_lemma (f : ℝ → ℝ) (hf : ContDiff ℝ 1 f) : 
   is_negligeable (f '' {x | deriv f x = 0}) := 
